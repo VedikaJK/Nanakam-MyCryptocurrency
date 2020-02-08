bee5f746d60be2554608a1807cd7b1c638dc674f
Vedika <vedika.j.kulkarni@gmail.com>
2020-02-09 00:24:31 +0530

    #4

--- sha-256.js
+++ sha-256.js
@@ -1,3 +1,13 @@
-const sha256=()=>{};
+const crypto = require('crypto'); //javascript module 'crypto' (== class)
+ 
+//this has a CreateHash function which can be used to create crypto objects
+
+const sha256=(...inputs)=>{              // javascripts spread operator for multiple no. of inputs
+
+        const hash=crypto.createHash('sha256');
+        hash.update(inputs.sort().join(' '));    //update accepts a string hence join
+                                                    // sort so that any order of inputs produces same o/p
+        return hash.digest('hex');
+}; 
 
 module.exports = sha256;
